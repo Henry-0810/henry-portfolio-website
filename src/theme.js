4c5d34d4115bcf2c8efbd45b4795683d36f56fb1
Henry-0810 <Mun.Li.Pan@students.ittralee.ie>
2023-09-11 00:18:34 +0100

    fonts done

--- src/theme.js
+++ src/theme.js
@@ -4,23 +4,27 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#0B132B', // Dark Blue
+      main: '#0B132B',
     },
     secondary: {
-      main: '#1C2541', // Navy Blue
+      main: '#EDEADE', 
     },
     background: {
-      default: '#3A506B', // Darker Blue
-    },
-    text: {
-      primary: '#5BC0BE', // Turquoise
-      secondary: '#6FFFE9', // Cyan
+      default: '#3A506B', 
     },
   },
   typography: {
-    fontFamily: 'Alice, sans-serif', // Use a suitable font family
+    fontFamily: 'Playfair Display, sans-serif', // Use a suitable font family
     h6: {
+      color: '#EDEADE', // Set text color
       fontSize: '35px', // Adjust heading font size
+      paddingLeft: '10px',
+      paddingBottom: '5px',
+    },
+    button: {
+      fontFamily: 'Raleway, sans-serif', // Use a suitable font family
+      color: '#EDEADE', // Set text color
+      fontSize: '20px', // Adjust heading font size
     },
     // Add styles for other headings and text as needed
   },
