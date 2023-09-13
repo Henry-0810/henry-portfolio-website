4e153816a941a8cbc7cbb0337f23a3e4eb323bdd
Henry-0810 <Mun.Li.Pan@students.ittralee.ie>
2023-09-13 16:21:07 +0100

    icons there

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,13 +1,41 @@
-import { Container, Paper, Typography } from "@mui/material";
+import React from "react";
+import { Container, Grid, Typography } from "@mui/material";
 import styles from "../CssStuff/Skills.module.css";
+import SvgIcon from "@mui/material/SvgIcon";
 
 const Skills = () => {
+  // Define an array of objects with icon names and corresponding icons
+  const skillsList = [
+    { name: "HTML", icon: <SvgIcon component="Html.svg" /> },
+    { name: "CSS", icon: <SvgIcon component="Css.svg" /> },
+    { name: "Javascript", icon: <SvgIcon component="Javascript.svg" /> },
+    { name: "ExpressJs", icon: <SvgIcon component="Express-js.svg" /> },
+    { name: "Azure DevOps", icon: <SvgIcon component="Azure.svg" /> },
+    { name: "Azure DevOps", icon: <SvgIcon component="Azure.svg" /> },
+    { name: "Azure DevOps", icon: <SvgIcon component="Azure.svg" /> },
+    { name: "Azure DevOps", icon: <SvgIcon component="Azure.svg" /> },
+    { name: "Azure DevOps", icon: <SvgIcon component="Azure.svg" /> },
+    { name: "Azure DevOps", icon: <SvgIcon component="Azure.svg" /> },
+    { name: "Azure DevOps", icon: <SvgIcon component="Azure.svg" /> },
+    { name: "Azure DevOps", icon: <SvgIcon component="Azure.svg" /> },
+    { name: "Azure DevOps", icon: <SvgIcon component="Azure.svg" /> },
+    { name: "Azure DevOps", icon: <SvgIcon component="Azure.svg" /> },
+    { name: "Azure DevOps", icon: <SvgIcon component="Azure.svg" /> },
+  ];
+
   return (
     <Container>
       <Typography variant="h4" className={styles.title}>
         I'm experienced with these technologies
       </Typography>
-      <Paper elevation={4} className={styles.surface}></Paper>
+      <Grid container spacing={3}>
+        {skillsList.map((skill, index) => (
+          <Grid item xs={4} key={index}>
+            <div className={styles.icon}>{skill.icon}</div>
+            <Typography variant="subtitle1">{skill.name}</Typography>
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   );
 };
